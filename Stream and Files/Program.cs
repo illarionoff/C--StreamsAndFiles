3683c2545f853c7d613545fbde2f8a66a68ba9a8
Afanasiy Illarionov <afanasiy.illarionov@dataart.com>
2020-01-31 14:35:10 +0300

    Add example

--- Stream and Files/Program.cs
+++ Stream and Files/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Stream_and_Files
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-   
+            using (var sw = new StreamWriter(@"C:\Temp\Text.txt", false, Encoding.UTF8)
+            ) //Create object only withing give scope. It will be destroyed byt dispose method
+            {
+                //Open stream
+                //Write
+                sw.Write("Привет");
+                sw.WriteLine("New Line");
+                //Close
+            }
+
+            using (var sr = new StreamReader(@"C:\Temp\Text.txt", Encoding.UTF8))
+            {
+                //Open stream
+                //Read
+                var result = sr.ReadToEnd();
+                Console.WriteLine(result);
+                //Close
+            }
         }
     }
 }
